410fe7f2a46ae05aef6b960df6a2e55ece330d83
moonyoungeun <pdu04223@naver.com>
2022-07-18 16:12:10 +0900

    220718

---
+++ jq.js
@@ -0,0 +1,44 @@
+// function showValue(){
+//     console.log($("input").val);
+// }
+
+// function showValue(){
+//     $("input").val("가나다라마바사");
+//     // document.querySelector("input")
+// }
+
+// $("span").text("가나다라마바사");
+
+// function span = document.querySelector
+
+// function up1(){
+//     $("input").val("가나다라마바사");
+// }
+
+// let inputText =document.querySelector(".text-input");
+
+// let writeBtn = document.querySelector("write-dtn");
+
+// let modifyBtn = document.querySelector("modify");
+
+// down.addEventListener("click",function(){
+//     inputText.value = "가나다라마바사";
+// })
+
+// let spanEl = document.querySelector("span");
+// function changeSpan(){ spanEl.textContent = inputText.value;
+
+// }
+
+// $(".write-dtn").on("click",function(){
+//     $("span").text($(".text-input").val());
+// })
+
+// $(".modityBtn").on("click", function(){
+//     $(".text-input").val()
+// })
+
+$(".text-input").on("click",function(){
+    $(".text-input").attr("type","button");
+    $(".text-input").attr("value","button");
+})
