e1b3872ee8a81a5810a71680d35ecf19ba127585
moonyoungeun <pdu04223@naver.com>
2022-07-20 13:27:58 +0900

    220720

--- ex_2.js
+++ ex_2.js
@@ -1,18 +1,35 @@
-let dateTable = document.querySelector("table");
-let dayDate = document.querySelector("#date");
-let content = document.querySelector("#content");
-let btn = document.querySelector("button")
+const calendar = document.querySelector("table");
+const date = document.querySelector("#date");
+const content = document.querySelector("#content");
+// let btn = document.querySelector("button")
 
-dateTable.addEventListener("click",function(e){
-    if(e.target.tagName === "p"){
+
+let target;
+
+calendar.addEventListener("click",function(e){
+    console.log("e-target",e.target.tagName);
+    if(e.target.tagName === "P"){
         date.value = e.target.textContent;
-        click = e.target.parentNode;
+        target = e.target.parentNode;
+    }else if (e.target.tagName === "TD"){
+        date.value = e.target.firstChild.textContent;
+        target = e.target;
     }
 })
 
 
-function writeSchedule() {
-    let div = document.createElement("div");
-    div.textContent = content.value;
-    click.append(div);
-}
+
+// dateTable.addEventListener("click",function(e){
+//     if(e.target.tagName === "p"){
+//         date.value = e.target.textContent;
+//         click = e.target.parentNode;
+//     }
+// });
+
+
+// function writeSchedule() {
+//     let div = document.createElement("div");
+//     div.textContent = content.value;
+//     click.append(div);
+// }
+
